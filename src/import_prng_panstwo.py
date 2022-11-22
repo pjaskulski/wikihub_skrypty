d192a7cc7963844046f6eb8c35157203b6c12ed4
Piotr Jaskulski <piotr.jaskulski@gmail.com>
2022-11-22 21:57:24 +0100

    autoryzacja i obsługa błędów

--- src/import_prng_panstwo.py
+++ src/import_prng_panstwo.py
@@ -8,7 +8,8 @@ from dotenv import load_dotenv
 from wikibaseintegrator import wbi_core
 from wikibaseintegrator.wbi_config import config as wbi_config
 from wikibaseintegrator import wbi_login
-from wikidariahtools import find_name_qid, element_search_adv, get_coord
+from wikibaseintegrator.wbi_exceptions import (MWApiError)
+from wikidariahtools import element_search_adv, get_coord, get_properties, get_elements
 from property_import import create_statement_data
 
 
@@ -21,8 +22,11 @@ wbi_config['WIKIBASE_URL'] = 'https://prunus-208.man.poznan.pl'
 env_path = Path(".") / ".env"
 load_dotenv(dotenv_path=env_path)
 
-BOT_LOGIN = os.environ.get('WIKIDARIAH_USER')
-BOT_PASSWORD = os.environ.get('WIKIDARIAH_PWD')
+# OAuth
+WIKIDARIAH_CONSUMER_TOKEN = os.environ.get('WIKIDARIAH_CONSUMER_TOKEN')
+WIKIDARIAH_CONSUMER_SECRET = os.environ.get('WIKIDARIAH_CONSUMER_SECRET')
+WIKIDARIAH_ACCESS_TOKEN = os.environ.get('WIKIDARIAH_ACCESS_TOKEN')
+WIKIDARIAH_ACCESS_SECRET = os.environ.get('WIKIDARIAH_ACCESS_SECRET')
 
 # pomiar czasu wykonania
 start_time = time.time()
@@ -30,54 +34,17 @@ start_time = time.time()
 WIKIBASE_WRITE = True
 
 # standardowe właściwości i elementy
-ok, p_instance_of = find_name_qid('instance of', 'property', strict=True)
-if not ok:
-    print("ERROR: brak właściwości 'instance of' w instancji Wikibase")
-    sys.exit(1)
-ok, p_stated_as = find_name_qid('stated as', 'property', strict=True)
-if not ok:
-    print("ERROR: brak właściwości 'stated as' w instancji Wikibase")
-    sys.exit(1)
-ok, p_coordinate = find_name_qid('coordinate location', 'property', strict=True)
-if not ok:
-    print("ERROR: brak właściwości 'coordinate location' w instancji Wikibase")
-    sys.exit(1)
-ok, p_reference_url = find_name_qid('reference URL', 'property', strict=True)
-if not ok:
-    print("ERROR: brak właściwości 'reference URL' w instancji Wikibase")
-    sys.exit(1)
-ok, p_inflectional_form = find_name_qid('inflectional form', 'property', strict=True)
-if not ok:
-    print("ERROR: brak właściwości 'inflectional form' w instancji Wikibase")
-    sys.exit(1)
-ok, p_locative_form = find_name_qid('locative form', 'property', strict=True)
-if not ok:
-    print("ERROR: brak właściwości 'locative form' w instancji Wikibase")
-    sys.exit(1)
-ok, p_adjective_form = find_name_qid('adjective form', 'property', strict=True)
-if not ok:
-    print("ERROR: brak właściwości 'adjective form' w instancji Wikibase")
-    sys.exit(1)
-ok, p_located_in_string = find_name_qid('located in (string)', 'property', strict=True)
-if not ok:
-    print("ERROR: brak właściwości 'located in (string)' w instancji Wikibase")
-    sys.exit(1)
-ok, p_located_in_country = find_name_qid('located in country', 'property', strict=True)
-if not ok:
-    print("ERROR: brak właściwości 'located in country' w instancji Wikibase")
-    sys.exit(1)
-ok, p_id_sdi = find_name_qid('id SDI', 'property', strict=True)
-if not ok:
-    print("ERROR: brak właściwości 'id SDI' w instancji Wikibase")
-    sys.exit(1)
+properties = get_properties(['instance of', 'stated as', 'reference URL', 'retrieved',
+                                'coordinate location', 'inflectional form', 'locative form',
+                                'adjective form', 'located in (string)', 'located in country',
+                                'id SDI'
+                                ])
 
-# elementy definicyjne
-q_country = 'Q86557'
-
+elements = get_elements(['country'])
 
 # wspólna referencja dla wszystkich deklaracji z PRNG
 references = {}
-references[p_reference_url] = 'https://mapy.geoportal.gov.pl/wss/service/PZGiK/PRNG/WFS/GeographicalNames'
+references[properties['reference URL']] = 'https://mapy.geoportal.gov.pl/wss/service/PZGiK/PRNG/WFS/GeographicalNames'
 
 
 # ----------------------------------- MAIN -------------------------------------
@@ -85,7 +52,13 @@ references[p_reference_url] = 'https://mapy.geoportal.gov.pl/wss/service/PZGiK/P
 if __name__ == '__main__':
 
     # logowanie do instancji wikibase
-    login_instance = wbi_login.Login(user=BOT_LOGIN, pwd=BOT_PASSWORD)
+    if WIKIBASE_WRITE:
+        login_instance = wbi_login.Login(consumer_key=WIKIDARIAH_CONSUMER_TOKEN,
+                                         consumer_secret=WIKIDARIAH_CONSUMER_SECRET,
+                                         access_token=WIKIDARIAH_ACCESS_TOKEN,
+                                         access_secret=WIKIDARIAH_ACCESS_SECRET,
+                                         token_renew_period=14400)
+
 
     xlsx_input = '../data_prng/PRNG_egzonimy_panstwo.xlsx'
     wb = openpyxl.load_workbook(xlsx_input)
@@ -101,7 +74,10 @@ if __name__ == '__main__':
         col_names[column[0].value] = nr_col
         nr_col += 1
 
-    for index, row in enumerate(ws.iter_rows(2, ws.max_row), start=1):
+    index = 0
+    max_row = ws.max_row
+    for row in ws.iter_rows(2, max_row):
+        index += 1
         # wczytanie danych z xlsx
         nazwa = row[col_names['nazwaGlown']].value
         if not nazwa:
@@ -129,7 +105,7 @@ if __name__ == '__main__':
         polozenie_t = row[col_names['polozenieT']].value
         wsp_geo = row[col_names['wspGeograf']].value
 
-        description_pl = 'państwo'
+        description_pl = 'kraj' # zamiast 'państwo'
         description_en = 'country'
 
         # przygotowanie struktur wikibase
@@ -137,32 +113,32 @@ if __name__ == '__main__':
         aliasy = []
 
         # instace of
-        statement = create_statement_data(p_instance_of, q_country, None, None, add_ref_dict=None)
+        statement = create_statement_data(properties['instance of'], elements['country'], None, None, add_ref_dict=None)
         if statement:
             data.append(statement)
 
         # współrzędne geograficzne
         if wsp_geo:
             coordinate = get_coord(wsp_geo)
-            statement = create_statement_data(p_coordinate, coordinate, None, None, add_ref_dict=references)
+            statement = create_statement_data(properties['coordinate location'], coordinate, None, None, add_ref_dict=references)
             if statement:
                 data.append(statement)
 
         # id SDI
         if idiip:
-            statement = create_statement_data(p_id_sdi, idiip, None, None, add_ref_dict=references)
+            statement = create_statement_data(properties['id SDI'], idiip, None, None, add_ref_dict=references)
             if statement:
                 data.append(statement)
 
         # nazwaGlow
         qualifiers = {}
         if odmiana_ngd:
-            qualifiers[p_inflectional_form] = odmiana_ngd
+            qualifiers[properties['inflectional form']] = odmiana_ngd
         if odmiana_ngm:
-            qualifiers[p_locative_form] = odmiana_ngm
+            qualifiers[properties['locative form']] = odmiana_ngm
         if odmiana_ngp:
-            qualifiers[p_adjective_form] = odmiana_ngp
-        statement = create_statement_data(p_stated_as, f'pl:"{nazwa}"', None, qualifiers, add_ref_dict=references)
+            qualifiers[properties['adjective form']] = odmiana_ngp
+        statement = create_statement_data(properties['stated as'], f'pl:"{nazwa}"', None, qualifiers, add_ref_dict=references)
         if statement:
             data.append(statement)
 
@@ -171,12 +147,12 @@ if __name__ == '__main__':
             aliasy.append(nazwa_dlug)
             qualifiers = {}
             if odmiana_ndd:
-                qualifiers[p_inflectional_form] = odmiana_ndd
+                qualifiers[properties['inflectional form']] = odmiana_ndd
             if odmiana_ndm:
-                qualifiers[p_locative_form] = odmiana_ndm
+                qualifiers[properties['locative form']] = odmiana_ndm
             if odmiana_ndp:
-                qualifiers[p_adjective_form] = odmiana_ndp
-            statement = create_statement_data(p_stated_as, f'pl:"{nazwa_dlug}"', None, qualifiers, add_ref_dict=references)
+                qualifiers[properties['adjective form']] = odmiana_ndp
+            statement = create_statement_data(properties['stated as'], f'pl:"{nazwa_dlug}"', None, qualifiers, add_ref_dict=references)
             if statement:
                 data.append(statement)
 
@@ -185,25 +161,25 @@ if __name__ == '__main__':
             aliasy.append(nazwa_obocz)
             qualifiers = {}
             if odmiana_nod:
-                qualifiers[p_inflectional_form] = odmiana_nod
+                qualifiers[properties['inflectional form']] = odmiana_nod
             if odmiana_nom:
-                qualifiers[p_locative_form] = odmiana_nom
+                qualifiers[properties['locative form']] = odmiana_nom
             if odmiana_nop:
-                qualifiers[p_adjective_form] = odmiana_nop
-            statement = create_statement_data(p_stated_as, f'pl:"{nazwa_obocz}"', None, qualifiers, add_ref_dict=references)
+                qualifiers[properties['adjective form']] = odmiana_nop
+            statement = create_statement_data(properties['stated as'], f'pl:"{nazwa_obocz}"', None, qualifiers, add_ref_dict=references)
             if statement:
                 data.append(statement)
 
         # nazwaHist
         if nazwa_hist:
             aliasy.append(nazwa_hist)
-            statement = create_statement_data(p_stated_as, f'pl:"{nazwa_hist}"', None, None, add_ref_dict=references)
+            statement = create_statement_data(properties['stated as'], f'pl:"{nazwa_hist}"', None, None, add_ref_dict=references)
             if statement:
                 data.append(statement)
 
         # polozenieT
         if polozenie_t:
-            statement = create_statement_data(p_located_in_string, polozenie_t, None, None, add_ref_dict=references)
+            statement = create_statement_data(properties['stated as'], polozenie_t, None, None, add_ref_dict=references)
             if statement:
                 data.append(statement)
 
@@ -220,13 +196,29 @@ if __name__ == '__main__':
                 wb_item.set_aliases(value_alias, 'pl')
 
         # wyszukiwanie po etykiecie
-        parameters = [(p_instance_of, q_country)]
+        parameters = [(properties['instance of'], elements['country'])]
         ok, item_id = element_search_adv(label_en, 'en', parameters)
         if not ok:
             if WIKIBASE_WRITE:
-                new_id = wb_item.write(login_instance, bot_account=True, entity_type='item')
-                if new_id:
-                    print(f'Dodano nowy element: {label_en} / {label_pl} = {new_id}')
+                test = 1
+                while True:
+                    try:
+                        new_id = wb_item.write(login_instance, bot_account=True, entity_type='item')
+                        print(f'Dodano nowy element: {label_en} / {label_pl} = {new_id}')
+                        break
+                    except MWApiError as wb_error:
+                        err_code = wb_error.error_msg['error']['code']
+                        message = wb_error.error_msg['error']['info']
+                        print(f'ERROR: {err_code}, {message}')
+                        # jeżeli jest to problem z tokenem to próba odświeżenia tokena i powtórzenie
+                        # zapisu, ale tylko raz, w razie powtórnego błędu bad token, skrypt kończy pracę
+                        if err_code in ['assertuserfailed', 'badtoken']:
+                            if test == 1:
+                                print('Generate edit credentials...')
+                                login_instance.generate_edit_credentials()
+                                test += 1
+                                continue
+                        sys.exit(1)
             else:
                 new_id = 'TEST'
                 print(f"Przygotowano dodanie elementu - {label_en} / {label_pl}  = {new_id}")
