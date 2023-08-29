c598cc9965bf81249c5309665dd20361f3e75220
Piotr Jaskulski <piotr.jaskulski@gmail.com>
2023-08-29 18:43:46 +0200

    poprawki importów

--- src/ahp_dekanaty.py
+++ src/ahp_dekanaty.py
@@ -31,23 +31,23 @@ WIKIDARIAH_ACCESS_SECRET = os.environ.get('WIKIDARIAH_ACCESS_SECRET')
 # pomiar czasu wykonania
 start_time = time.time()
 
-WIKIBASE_WRITE = True
+WIKIBASE_WRITE = False
 
 # standardowe właściwości i elementy (P i Q wyszukiwane w wikibase raz i trzymane w słownikach)
 properties = get_properties(['instance of', 'stated as', 'reference URL', 'retrieved',
-                             'point in time', 'part of', 'has part or parts'
+                             'point in time', 'part of', 'has part or parts',
+                             'refine date', 'stated in'
                             ])
 
-elements = get_elements(['deanery (Roman Catholic Church)',
-                         'deaconry (Roman Catholic Church)',
-                         'provostship (Roman Catholic Church)',
-                         'archdeaconry (Roman Catholic Church)',
-                         'territory (Roman Catholic Church)',
-                         'diocese (Roman Catholic Church)'])
+elements = get_elements(['deanery (Latin Church)',
+                         'deaconry (Latin Church)',
+                         'provostship (Latin Church)',
+                         'archdeaconry (Latin Church)',
+                         'territory (Latin Church)',
+                         'diocese (Latin Church)',
+                         'second half'])
 
-# wspólna referencja dla wszystkich deklaracji
-references = {}
-references[properties['reference URL']] = 'https://atlasfontium.pl/ziemie-polskie-korony/'
+
 
 
 # ------------------------------------MAIN -------------------------------------
@@ -55,12 +55,11 @@ references[properties['reference URL']] = 'https://atlasfontium.pl/ziemie-polski
 if __name__ == '__main__':
 
     # logowanie do instancji wikibase
-    if WIKIBASE_WRITE:
-        login_instance = wbi_login.Login(consumer_key=WIKIDARIAH_CONSUMER_TOKEN,
-                                         consumer_secret=WIKIDARIAH_CONSUMER_SECRET,
-                                         access_token=WIKIDARIAH_ACCESS_TOKEN,
-                                         access_secret=WIKIDARIAH_ACCESS_SECRET,
-                                         token_renew_period=14400)
+    login_instance = wbi_login.Login(consumer_key=WIKIDARIAH_CONSUMER_TOKEN,
+                                        consumer_secret=WIKIDARIAH_CONSUMER_SECRET,
+                                        access_token=WIKIDARIAH_ACCESS_TOKEN,
+                                        access_secret=WIKIDARIAH_ACCESS_SECRET,
+                                        token_renew_period=14400)
 
     file_name = Path('..') / 'data' / 'ahp_dekanaty.csv'
     with open(file_name, 'r', encoding='utf-8') as f:
@@ -68,11 +67,14 @@ if __name__ == '__main__':
     lines = [line.strip() for line in lines]
 
     references = {}
-    references[properties['reference URL']] = 'https://atlasfontium.pl/ziemie-polskie-korony/'
-    qualifiers = {}
-    qualifiers[properties['point in time']] = '+1600-00-00T00:00:00Z/9'
+    references[properties['stated in']] = 'Q234031' # referencja do elementu AHP w instancji testowej!
+    references[properties['retrieved']] = '2023-06-15'
 
-    instance_of = elements['deanery (Roman Catholic Church)']
+    qualifiers = {}
+    qualifiers[properties['point in time']] = '+1600-00-00T00:00:00Z/7' # XVI wiek
+    qualifiers[properties['refine date']] = elements['second half']     # druga połowa
+
+    instance_of = elements['deanery (Latin Church)']
 
     for line in lines:
         t_line = line.split(',')
@@ -84,7 +86,7 @@ if __name__ == '__main__':
         label_pl = f"dekanat {dekanat_label}"
         label_en = f"deanery {dekanat_label}"
         description_pl = "dekanat (jednostka w systemie administracji kościelnej: Kościół katolicki ob. łacińskiego, wg Atlasu Historycznego Polski, stan na 2 poł. XVI wieku)"
-        description_en = "deanery (unit in the religious administrative system: Roman Catholic Church, according to the Historical Atlas of Poland, as of the 2nd half of the XVIth century)"
+        description_en = "deanery (unit in the religious administrative system: Latin Church, according to the Historical Atlas of Poland, as of the 2nd half of the XVIth century)"
 
         # przygotowanie struktur wikibase
         data = []
@@ -105,30 +107,30 @@ if __name__ == '__main__':
 
         # part of (archidiakonat lub diecezja)
         if archidiakonat:
-            parameters = [(properties['instance of'], elements['archdeaconry (Roman Catholic Church)'])]
+            parameters = [(properties['instance of'], elements['archdeaconry (Latin Church)'])]
             ok, master_qid = element_search_adv(f'archdeaconry {archidiakonat}', 'en', parameters)
 
             # może to deaconry, provostship, territory
             if not ok:
                 if archidiakonat == 'Kielce Dz':
                     label_archidiakonat = 'deaconry Kielce'
-                    element_archidiakonat = elements['deaconry (Roman Catholic Church)']
+                    element_archidiakonat = elements['deaconry (Latin Church)']
                 elif archidiakonat == 'Kielce Pr':
                     label_archidiakonat = 'provostship Kielce'
-                    element_archidiakonat = elements['provostship (Roman Catholic Church)']
+                    element_archidiakonat = elements['provostship (Latin Church)']
                 elif archidiakonat == 'Tarnów Pr':
                     label_archidiakonat = 'provostship Tarnów'
-                    element_archidiakonat = elements['provostship (Roman Catholic Church)']
+                    element_archidiakonat = elements['provostship (Latin Church)']
                 elif archidiakonat == 'Wieluń Ter':
                     label_archidiakonat = 'territory Wieluń'
-                    element_archidiakonat = elements['territory (Roman Catholic Church)']
+                    element_archidiakonat = elements['territory (Latin Church)']
                 else:
                     print('ERROR: nie znaleziono jednostki nadrzędnej:', g_dekanat, archidiakonat, diecezja)
 
                 parameters = [(properties['instance of'], element_archidiakonat)]
                 ok, master_qid = element_search_adv(label_archidiakonat, 'en', parameters)
         else:
-            parameters = [(properties['instance of'], elements['diocese (Roman Catholic Church)'])]
+            parameters = [(properties['instance of'], elements['diocese (Latin Church)'])]
             ok, master_qid = element_search_adv(f'diocese {diecezja}', 'en', parameters)
 
         if ok:
@@ -159,7 +161,7 @@ if __name__ == '__main__':
                 while True:
                     try:
                         new_id = wb_item.write(login_instance, bot_account=True, entity_type='item')
-                        print(f'Dodano nowy element: {label_en} / {label_pl} = {new_id}')
+                        print(f'Dodano:  # [https://prunus-208.man.poznan.pl/wiki/Item:{new_id} {label_en} / {label_pl}]')
 
                         # uzupełnienie archidiakonatu lub diecezji
                         if master_qid != 'NOT FOUND':
@@ -191,6 +193,6 @@ if __name__ == '__main__':
                         sys.exit(1)
             else:
                 new_id = 'TEST'
-                print(f"Przygotowano dodanie elementu - {label_en} / {label_pl}  = {new_id}")
+                print(f"Przygotowano dodanie:  # [https://prunus-208.man.poznan.pl/wiki/Item:{new_id} {label_en} / {label_pl}]")
         else:
-            print(f'Element: {label_en} / {label_pl} już istnieje: {item_id}')
+            print(f'Element:  # [https://prunus-208.man.poznan.pl/wiki/Item:{item_id} {label_en} / {label_pl}] już istnieje.')
